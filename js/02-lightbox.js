021a4fbdd086a6dc9ee1b0c3b8a5dd398bd315ff
Andrii Sadovyi <sadoviya@gmail.com>
2023-05-29 20:12:13 +0300

    second task complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const containerRef = document.querySelector(".gallery");
+containerRef.innerHTML = createGalleryItem(galleryItems);
+
+function createGalleryItem(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}"
+      alt="${description}"
+      title="${description}">
+      </a>`
+    )
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
