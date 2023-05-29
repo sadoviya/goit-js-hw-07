3ce73d0c26ddb592bb1df73241d08483b0266005
Andrii Sadovyi <sadoviya@gmail.com>
2023-05-29 19:48:49 +0300

    first task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const containerRef = document.querySelector(".gallery");
+containerRef.addEventListener("click", getOriginalUrl);
+
+function getOriginalUrl(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  openModalWindow(evt);
+}
+
+const createCardMarkup = createItemsList(galleryItems);
+containerRef.innerHTML = createCardMarkup;
+
+function createItemsList(items) {
+  return items
+    .map(
+      ({ original, preview, description }) =>
+        `<a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}"
+        data-source="${original}" alt="${description}">
+        </a>`
+    )
+    .join("");
+}
+
+function openModalWindow(evt) {
+  const modal = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}">`,
+    {
+      onShow: (modal) =>
+        document.body.addEventListener("keydown", onEscKeyPress),
+      onClose: (modal) =>
+        document.body.removeEventListener("keydown", onEscKeyPress),
+    }
+  );
+  modal.show();
+
+  function onEscKeyPress(evt) {
+    if (evt.code === "Escape") {
+      modal.close();
+    }
+  }
+}
